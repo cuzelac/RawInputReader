4be1ac15aa773e96a77450e69d9102647644dd6a
Chris Uzelac <cuzelac@gmail.com>
2021-03-05 17:40:54 -0800

    Add project files.

---
+++ RawInputReader/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using Newtonsoft.Json;
+using HidSharp;
+
+
+namespace RawInputReader
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("SCREW YOU BUDDY");
+            var list = DeviceList.Local;
+            var allDeviceList = list.GetAllDevices();
+
+            Console.WriteLine(
+                FormatJson(
+                  JsonConvert.SerializeObject(allDeviceList)
+                ));
+        }
+
+        // Console.WriteLine(FormatJson("{'a': 1, 'b': [2,3,4]}"));
+        private static string FormatJson(string json)
+        {
+            return JsonConvert.SerializeObject(
+                JsonConvert.DeserializeObject(json),
+                Formatting.Indented);
+        }
+    }
+}
